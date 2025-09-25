daba4d235669fa9a3147f1f83409b487481df9f2
dottah107 <arcadeaddicts07@gmail.com>
2025-09-25 15:30:47 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+﻿self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('aa-v1').then((c) =>
+      c.addAll(['/', '/assets/logo.png'])
+    )
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
