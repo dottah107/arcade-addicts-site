a48932aca0add9c157101e7ad1cdc82d93fb9111
dottah107 <arcadeaddicts07@gmail.com>
2025-09-25 20:08:45 -0400

    Force mobile hamburger menu to include Login/Home/Tournaments/Lounge/Register/Pay/Discord/Contacts; bump SW cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 ﻿self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('aa-v4').then((c) =>
+    caches.open('aa-v5').then((c) =>
       c.addAll(['/', '/assets/logo.png'])
     )
   );
@@ -8,5 +8,6 @@
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
+
 
 
