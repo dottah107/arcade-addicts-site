71548f80a9b664fa20163c88fab85bfe15be896f
dottah107 <arcadeaddicts07@gmail.com>
2025-11-06 04:39:28 -0500

    guad

---
+++ assets/js/guard.js
@@ -0,0 +1,34 @@
+﻿(() => {
+  const sb = window.supabaseClient;
+  if (!sb) return;
+
+  // Which pages require login?
+  const protectedPaths = [
+    /\/members/i,
+    /\/account/i,
+    /\/friends/i,
+    /\/tournaments/i,
+    /\/aaalounge/i,
+    /\/lounge/i,
+    /\/pay/i
+  ];
+
+  const needsAuth = protectedPaths.some((re) => re.test(location.pathname));
+  if (needsAuth) {
+    (async () => {
+      const { data: { user } } = await sb.auth.getUser();
+      if (!user) location.href = "/login/";
+    })();
+  }
+
+  // Optional logout link handler
+  const logoutEl = document.getElementById("logout");
+  if (logoutEl && !logoutEl.dataset.wired) {
+    logoutEl.dataset.wired = "1";
+    logoutEl.addEventListener("click", async (e) => {
+      e.preventDefault();
+      await sb.auth.signOut();
+      location.href = "/login/";
+    });
+  }
+})();
